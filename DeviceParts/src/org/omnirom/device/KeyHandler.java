cdc504f5f3a878aacc20dcca2b7502bdd0e13746
Adhitya Mohan <me@adhityamohan.in>
2020-04-13 09:21:33 +0530

    Replace with our utils file

--- DeviceParts/src/org/omnirom/device/KeyHandler.java
+++ DeviceParts/src/org/omnirom/device/KeyHandler.java
@@ -61,7 +61,7 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.os.DeviceKeyHandler;
 import com.android.internal.util.ArrayUtils;
-import com.android.internal.util.aosip.aosipUtils;
+import com.android.internal.util.nitrogen.NitrogenUtils;
 import com.android.internal.statusbar.IStatusBarService;
 
 public class KeyHandler implements DeviceKeyHandler {
@@ -581,19 +581,19 @@ public class KeyHandler implements DeviceKeyHandler {
             mAudioManager.adjustSuggestedStreamVolume(AudioManager.ADJUST_LOWER,AudioManager.USE_DEFAULT_STREAM_TYPE,AudioManager.FLAG_SHOW_UI);
             return true;
         } else if (value.equals(AppSelectListPreference.BROWSE_SCROLL_DOWN_ENTRY)) {
-            aosipUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_DOWN);
+            NitrogenUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_DOWN);
             return true;
         } else if (value.equals(AppSelectListPreference.BROWSE_SCROLL_UP_ENTRY)) {
-            aosipUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_UP);
+            NitrogenUtils.sendKeycode(KeyEvent.KEYCODE_PAGE_UP);
             return true;
         } else if (value.equals(AppSelectListPreference.NAVIGATE_BACK_ENTRY)) {
-            aosipUtils.sendKeycode(KeyEvent.KEYCODE_BACK);
+            NitrogenUtils.sendKeycode(KeyEvent.KEYCODE_BACK);
             return true;
         } else if (value.equals(AppSelectListPreference.NAVIGATE_HOME_ENTRY)) {
-            aosipUtils.sendKeycode(KeyEvent.KEYCODE_HOME);
+            NitrogenUtils.sendKeycode(KeyEvent.KEYCODE_HOME);
             return true;
         } else if (value.equals(AppSelectListPreference.NAVIGATE_RECENT_ENTRY)) {
-            aosipUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
+            NitrogenUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
             return true;
         }
         return false;
