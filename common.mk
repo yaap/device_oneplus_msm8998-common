22626e4065a33e77e05c16ef758ee346374b6b4c
Adhitya Mohan <me@adhityamohan.in>
2020-09-13 00:09:25 +0530

    msm8998-common: Build gaaps

--- common.mk
+++ common.mk
@@ -23,6 +23,7 @@
 #
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Overlays
